488287a7b8a43230b41c79e3fbb5031489a34e17
jeffbonson <jeffbonson@gmail.com>
2024-05-24 19:06:13 +0530

    version 1.2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bookstore - React - 1.1
+          Bookstore - React - 1.2
         </p>
         
       </header>
